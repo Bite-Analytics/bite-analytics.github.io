56f740d3384e312bf57f72a7c2b560e9a671194f
Bite-Analytics <Bite-Analytics@users.noreply.github.com>
2025-06-29 13:24:34 +0000

    deploy: 2329fa23f0cba711c73c094dc75376f2d0667c2a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Post: Notice",
-        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
-        "tags": ["Post Formats","notice"],
-        "url": "/blog/post-notice/",
-        "teaser": null
-      },{
         "title": "Post: Quote",
         "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
         "tags": ["Post Formats","quote"],
