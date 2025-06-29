268cf1f88a15cad00c9223eafb5cb2ad1ad6e20b
Bite-Analytics <Bite-Analytics@users.noreply.github.com>
2025-06-29 13:24:13 +0000

    deploy: 79ea4a8954ca22e644ac874d127f460ea916d546

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-modified/",
-        "teaser": null
-      },{
         "title": "Post: Standard",
         "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
         "tags": ["Post Formats","readability","standard"],
