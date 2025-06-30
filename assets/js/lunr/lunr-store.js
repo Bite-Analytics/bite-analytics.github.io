6c720879587ddde1cb5b7669e8dc07629c6ab377
Bite-Analytics <Bite-Analytics@users.noreply.github.com>
2025-06-30 16:47:26 +0000

    deploy: 68d609d61dc7e7a914e12d464594243a571a164a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "/Power-BI-Microsoft-Fabric/",
         "teaser": null
       },{
-        "title": "Power BI + Microsoft Fabric: A Unified Analytics Experience",
-        "excerpt":"By Manpreet Ragi, Analytics Consultant Microsoft Fabric is reshaping the analytics landscape—and Power BI is at the heart of this transformation. As organizations demand faster insights, tighter governance, and more seamless collaboration, Fabric delivers a unified platform that empowers data professionals and business users alike. 🚀 What Is Microsoft Fabric?...","categories": [],
-        "tags": [],
-        "url": "/welcome-to-bite-analytics/",
-        "teaser": null
-      },{
         "title": "Microsoft Fabric Medallion Architectures",
         "excerpt":"🧠 OneLake vs Lakehouses: What’s the Difference? OneLake is the unified, organization-wide data lake in Microsoft Fabric—think of it as the single storage system for your entire data estate. Lakehouses are logical containers within OneLake that organize and manage data using a structured format (like tables). Each lakehouse can represent...","categories": ["blog"],
         "tags": [],
