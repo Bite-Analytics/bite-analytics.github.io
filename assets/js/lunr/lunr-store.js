f24cb8091f4cf70a8ac4b66ef2b0bcb3e1d2dd40
Bite-Analytics <Bite-Analytics@users.noreply.github.com>
2025-06-30 16:42:41 +0000

    deploy: 027834c758d3c9a49733011084406a5a85015f6a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,10 +5,10 @@ var store = [{
         "url": "/Power-BI-Microsoft-Fabric/",
         "teaser": null
       },{
-        "title": "Welcome to Bite Analytics!",
-        "excerpt":"👋 Hey there! Welcome to Bite Analytics — your new home for smart, simple insights into Business Intelligence and Artificial Intelligence. This is our very first blog post, and it’s all about what you can expect from the Bite Analytics journey. 🚀 Why Bite Analytics? We believe data shouldn’t be...","categories": ["BI","AI"],
-        "tags": ["intro","youtube","BI","AI"],
-        "url": "/bi/ai/welcome-to-bite-analytics/",
+        "title": "Power BI + Microsoft Fabric: A Unified Analytics Experience",
+        "excerpt":"By Manpreet Ragi, Analytics Consultant Microsoft Fabric is reshaping the analytics landscape—and Power BI is at the heart of this transformation. As organizations demand faster insights, tighter governance, and more seamless collaboration, Fabric delivers a unified platform that empowers data professionals and business users alike. 🚀 What Is Microsoft Fabric?...","categories": [],
+        "tags": [],
+        "url": "/welcome-to-bite-analytics/",
         "teaser": null
       },{
         "title": "Microsoft Fabric Medallion Architectures",
